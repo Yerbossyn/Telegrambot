4496d2092580471f7473f3701100fbdf711091eb
Yerbossyn <73870324+Yerbossyn@users.noreply.github.com>
2020-11-29 21:03:17 +0600

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,11 @@
+import telebot
+import config
+import pb
+import datetime
+import pytz
+import json
+import traceback
+
+
+P_TIMEZONE = pytz.timezone(config.TIMEZONE)
+TIMEZONE_COMMON_NAME = config.TIMEZONE_COMMON_NAME
